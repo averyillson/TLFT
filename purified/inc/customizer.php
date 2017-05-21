de89e2462954e0358657c699dd0d6a170559785e
Anthony Solis <anthonysolis@Anthonys-iMac.local>
2017-05-21 14:48:47 -0400

    master: first commit

---
+++ purified/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * purified Theme Customizer
+ *
+ * @package purified
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function purified_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'purified_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function purified_customize_preview_js() {
+	wp_enqueue_script( 'purified_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'purified_customize_preview_js' );
